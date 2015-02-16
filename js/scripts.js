ff5db9d122a53fc8daae07df56c8715e0e9f532d
Sarah Joy Calpo & Grace Mekarski <student@epicodus.com>
2015-02-16 12:30:03 -0800

    add compelete favorites section

--- js/scripts.js
+++ js/scripts.js
@@ -7,19 +7,30 @@ $(function(){
     var inputtedAddress = $("input#new-address").val();
     var newContact = {firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
 
-    $("ul#contact-list").append("<li><i class='fa-li fa fa-home'></i><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span><p>Add to Favorites?<input type='checkbox' class='favorite' value='Favorite's List?'></p></li>");
+    $("ul#contact-list").append("<li><i class='fa-li fa fa-home'></i><span class='contact link'>" + newContact.firstName + " " + newContact.lastName + "</span><p>Add to Favorites?<input type='checkbox' class='favorite' value='Favorite's List?'></p></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-address").val("");
 
-    $(".contact").last().click(function() {
+    var addLink = function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
       $(".address").text(newContact.address);
-    });
+    };
+
+
+    $(".contact").last().click(addLink);
+    //   function() {
+    //   $("#show-contact").show();
+    //   $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+    //   $(".first-name").text(newContact.firstName);
+    //   $(".last-name").text(newContact.lastName);
+    //   $(".address").text(newContact.address);
+    // });
+
 
     $("ul#contact-list li").click(function() {
       $("ul#contact-list li").removeClass("highlight");
@@ -29,10 +40,11 @@ $(function(){
     $("input.favorite").last().on("click", function() {
       var thisCheck = $(this);
       if (thisCheck.is(':checked')) {
-      $("ul#favorite-list").append("<li class=" + newContact.firstName + "><i class='fa-li fa fa-home'></i>" + newContact.firstName + "</li>");
-    } else {
-      $("ul#favorite-list").children("li." + newContact.firstName).remove();
-    }
+        $("ul#favorite-list").append("<li class='" + newContact.firstName + "'><i class='fa-li fa fa-smile-o'></i><span class='favorite link'>" + newContact.firstName + "</span></li>");
+        $(".favorite").last().click(addLink);
+      } else {
+        $("ul#favorite-list").children("li." + newContact.firstName).remove();
+      }
     });
   });
 });
