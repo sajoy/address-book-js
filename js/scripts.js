e299112811ab7e0eeee5584195094f9295479654
Whitney Paige Reans & Sarah Joy Calpo <student@epicodus.com>
2015-02-17 10:00:33 -0800

    add new address object

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,36 @@ $(function(){
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
-    var newContact = {firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+    var inputtedStreet = $("input#new-street").val();
+    var inputtedCity = $("input#new-city").val();
+    var inputtedState = $("input#new-state").val();
+    var inputtedZip = $("input#new-zip").val();
+    var newAddress = { street: inputtedStreet,
+                            city: inputtedCity,
+                            state: inputtedState,
+                            zip: inputtedZip,
+                            fullAddress: function() {
+                             return this.street + "<br> " + this.city + ", " + this.state + ", " + this.zip;
+                            }
+                     };
+
+    var newContact = {firstName: inputtedFirstName, lastName: inputtedLastName, address: newAddress.fullAddress() };
 
     $("ul#contact-list").append("<li><i class='fa-li fa fa-home'></i><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
-    $("input#new-address").val("");
+    $("input#new-city").val("");
+    $("input#new-street").val("");
+    $("input#new-state").val("");
+    $("input#new-zip").val("");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
+      $(".address").append(newContact.address);
     });
 
     $("ul#contact-list li").click(function() {
